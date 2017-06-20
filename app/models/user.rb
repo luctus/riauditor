fd37b53307815c0f9614b8297538f3da6c520090
Gustavo Garcia <luctus@gmail.com>
2017-06-20 00:06:35 -0400

    ADD: update background

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  #has_many :accounts
+  has_many :accounts
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
